a9813d46a4da2fa8d02f997379703b11b63a8726
Aleksandar Medarevic <39373622+AleksandarMedarevic@users.noreply.github.com>
2023-02-12 21:00:04 +0100

    Add files via upload

---
+++ data.js
@@ -0,0 +1,145 @@
+export const filmsArchive = [
+    {
+        genreTag: ["comedy", "family"],
+        isSerbian: false,
+        image: "alf.jpg",
+        alt: "Alf",
+        start: 1986,
+    },
+    {
+        genreTag: ["history", "comedy", "war"],
+        isSerbian: false,
+        image: "alo-alo.jpg",
+        alt: "Allo, allo",
+        start: 1982,
+    },
+    {
+        genreTag: ["mistery", "triller"],
+        isSerbian: true,
+        image: "beleznica-profesora.jpg",
+        alt: "Beležnica profesora Miškovića",
+        start: 2021,
+    },
+    {
+        genreTag: ["drama", "comedy", "crime"],
+        isSerbian: false,
+        image: "death-in-paradise.jpg",
+        alt: "Death in paradise",
+        start: 2011,
+    },
+    {
+        genreTag: ["crime", "biography", "drama"],
+        isSerbian: false,
+        image: "escobar.jpeg",
+        alt: "Pablo Escobar: El Patrón del Mal",
+        start: 2022,
+    },
+    {
+        genreTag: ["drama", "mistery", "crime"],
+        isSerbian: false,
+        image: "hobotnica.jpg",
+        alt: "La Piovra",
+        start: 1987,
+    },
+    {
+        genreTag: ["action", "crime", "mistery"],
+        isSerbian: false,
+        image: "inspector-rex.jpg",
+        alt: "Kommissar Rex",
+        start: 1987,
+    },
+    {
+        genreTag: ["comedy"],
+        isSerbian: false,
+        image: "mucke.jpg",
+        alt: "Only Fools and Horses",
+        start: 1982,
+    },
+    {
+        genreTag: ["drama", "mistery", "crime"],
+        isSerbian: false,
+        image: "poaro.jpg",
+        alt: "Poirot",
+        start: 2002,
+    },
+    {
+        genreTag: ["family", "crime", "comedy"],
+        isSerbian: true,
+        image: "policajac-s-petlovog-brda.jpg",
+        alt: "Policajac s Petlovog Brda",
+        start: 1993,
+    },
+    {
+        genreTag: ["war", "drama"],
+        isSerbian: true,
+        image: "salas-u-malom-ritu.jpg",
+        alt: "Salaš u malom ritu",
+        start: 1969,
+    },
+    {
+        genreTag: ["comedy","war"],
+        isSerbian: true,
+        image: "slozna-braca.jpg",
+        alt: "Složna Braća",
+        start: 1995,
+    },
+    {
+        genreTag: ["comedy", "drama"],
+        isSerbian: true,
+        image: "srecni-ljudi.jpg",
+        alt: "Srećni ljudi",
+        start: 1992,
+    },
+    {
+        genreTag: ["action", "crime", "triller"],
+        isSerbian: true,
+        image: "ubice-mog-oca.jpg",
+        alt: "Ubice mog oca",
+        start: 2016,
+    },
+    {
+        genreTag: ["history", "crime", "drama"],
+        isSerbian: false,
+        image: "vienna-blood.jpg",
+        alt: "Vienna Blood",
+        start: 2019,
+    },
+    {
+        genreTag: ["biography", "history", "drama"],
+        isSerbian: true,
+        image: "vuk-karadzic.jpg",
+        alt: "Vuk Karadžić",
+        start: 1989,
+    },
+    {
+        genreTag: ["biography", "history", "drama"],
+        isSerbian: true,
+        image: "kraj-dinastije.jpg",
+        alt: "Kraj dinastije Obrenović",
+        start: 1995,
+    },
+    {
+        genreTag: ["action", "mistery", "drama"],
+        isSerbian: false,
+        image: "squid-game.jpg",
+        alt: "Squid Game",
+        start: 2021,
+    },
+]
+
+function getFilmsGenre(films){
+    const genreArray=[]
+    for (let film of films){
+        for (let genre of film.genreTag){
+             genreArray.push(genre)
+             
+        }
+    } return genreArray
+    }
+
+function renderGenreArray(films){
+    const genres=getFilmsGenre(films)
+    console.log(genres)
+}
+
+renderGenreArray(filmsArchive)
---
+++ index.js
@@ -0,0 +1,128 @@
+// javascript
+
+import {filmsArchive} from "/data.js"
+const genreRadios = document.getElementById("genre-radios")
+let serbianOnly= document.getElementById(`serbian-only`)
+const choseBtn= document.getElementById(`chose-btn`)
+let genreId = ``
+const memeModalInner = document.getElementById('meme-modal-inner')
+const memeModal = document.getElementById('meme-modal')
+
+
+
+
+function getFilmsGenre(films){
+    const genreArray=[]
+    for (let film of films){
+        for (let genre of film.genreTag){
+             if (!genreArray.includes(genre)) {
+              genreArray.push(genre)  
+             }           
+        }
+    } return genreArray
+}
+
+function renderGenreArray(films){
+    let radioItem=``
+    const genres=getFilmsGenre(films)
+    for (let genre of genres) {
+        radioItem +=
+        `<div class="separate-genre" id="separate-genre">
+            <label for=${genre} class="labelBtn"> ${genre} </label>
+            <input type="radio" class="radio-button" name="radio-button" 
+         id=${genre} value=${genre}>
+        </div>`
+    }
+    genreRadios.innerHTML=radioItem
+}
+
+renderGenreArray(filmsArchive)
+
+
+genreRadios.addEventListener(`change`,function(e){
+    const genreItems = document.getElementsByClassName("separate-genre")
+    for (let genreItem of genreItems) {
+        genreItem.classList.remove(`chosed`)
+    }    
+    document.getElementById(e.target.id).parentElement.classList.add(`chosed`)
+})
+
+//is Serbian tv show by clicking
+
+function isSerbian() {
+    if (document.querySelector(`input[type="checkbox"]:checked`)){
+        serbianOnly=true
+        console.log(`is it Serbian show`,serbianOnly)
+    } else {
+        serbianOnly=false
+        console.log(`is it Serbian show`, serbianOnly)
+    } 
+
+    return serbianOnly
+}
+
+// chose genre
+
+function choseYourGenre(){
+     if (document.querySelector(`input[type="radio"]:checked`)){
+        genreId=document.querySelector(`input[type="radio"]:checked`).value
+        console.log(`izabrani zandr`, genreId)  
+     } else {
+        console.log('Choose your genre')
+        genreId=document.querySelector(`input[type="radio"]`).value
+    }
+    return genreId
+}
+
+function createChosedShowsArray(){
+    const showsGenreArray= filmsArchive.filter(function(show){
+         if (serbianOnly) {
+          return show.genreTag.includes(genreId) && show.isSerbian   
+         } else {
+          return show.genreTag.includes(genreId)
+         }         
+        })
+    //console.log(`niz`,showsGenreArray) 
+    return showsGenreArray
+}
+
+
+function getSingleShow(){
+    const seriesArray= createChosedShowsArray()
+    let item = Math.floor(Math.random()*seriesArray.length)
+    return (seriesArray[item])
+}
+
+function renderShow(){
+    const oneSerie = getSingleShow()
+    memeModal.innerHTML += 
+    `<div class="serija">
+    <img 
+        class="serie-img" 
+        src="./images/${oneSerie.image}"
+        alt="${oneSerie.alt}"
+        >
+        <h3>${oneSerie.alt}</h3>
+        <div>`
+    memeModal.style.display= `flex`    
+    
+}
+
+
+choseBtn.addEventListener('click', function(){ 
+    isSerbian()
+    choseYourGenre()
+    createChosedShowsArray()
+    getSingleShow()
+    renderShow()  
+})
+
+//close Btn
+
+let closeBtn = document.getElementById("close-btn")
+console.log(closeBtn)
+
+closeBtn.addEventListener("click", function(){
+    //memeModal.style.display= "none"
+    console.log(`aaaa`)
+})
